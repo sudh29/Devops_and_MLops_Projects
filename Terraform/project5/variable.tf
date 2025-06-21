ddfc75adfb9ba41480c21780796aaf77a876626e
sudh29 <sudhanshuiet15@gmail.com>
2025-06-21 12:53:59 +0000

    Fifth project for the variable use cases.

---
+++ Terraform/project5/variable.tf
@@ -0,0 +1,13 @@
+variable username {
+    type = string
+}
+
+variable userid {
+    type = number
+    default = 123
+}
+
+variable users {
+    type = list(string)
+    default = ["user1", "user2", "user3"]
+}
