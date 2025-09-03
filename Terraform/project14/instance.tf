2954fc46a7ad4a669b073526d6c1d6c135995a7c
sudh29 <sudhanshuiet15@gmail.com>
2025-09-03 12:54:33 +0000

    Add the project14 for backend use in terraform.

---
+++ Terraform/project14/instance.tf
@@ -0,0 +1,29 @@
+terraform {
+  backend "s3" {
+    bucket         = "terrabackendtest"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terrabackendtest-table"
+  }
+}
+
+variable "access_key" {
+  type        = string
+  description = "AWS access key"
+}
+
+variable "secret_key" {
+  type        = string
+  description = "AWS secret key"
+}
+
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0f918f7e67a3323f0"
+  instance_type = "t3.micro"
+}
